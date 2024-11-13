b177b3501bcc62b481ff02ae86b4074b80cda165
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-13 18:33:23 +0100

    get current year using js

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,7 +5,7 @@
 <template>
   <footer class="bg-white border-t border-gray-200 px-4 lg:px-6 py-2.5">
     <div class="w-full mx-auto max-w-screen-xl py-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center">© 2023 <NuxtLink to="/" class="hover:underline">CV Generator™</NuxtLink>. All Rights Reserved.
+      <span class="text-sm text-gray-500 sm:text-center">© {{ new Date().getFullYear() }} <NuxtLink to="/" class="hover:underline">CV Generator™</NuxtLink>. All Rights Reserved.
     </span>
       <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
         <li>
