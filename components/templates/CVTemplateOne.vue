5d25960a8f99e5b0a89cf8d646f7d46339355bcf
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-13 18:18:10 +0100

    remove data from CVTemplateOne

--- components/templates/CVTemplateOne.vue
+++ components/templates/CVTemplateOne.vue
@@ -5,32 +5,13 @@ const documentStore = useDocumentStore();
 </script>
 
 <template>
-  <div id="preview" class="bg-white rounded">
+  <div id="preview" class="max-w-2xl mx-auto h-full max-h-[800px] bg-white rounded">
     <div class="flex grow h-full">
       <div class="p-4 flex-[3_1_0%] border-r">
-        <div class="mb-4">
-          <div class="font-bold">
-            Personal Information
-          </div>
-          <div v-if="documentStore.document.email">
-            <div>E-mail</div>
-            <div>{{ documentStore.document.email }}</div>
-          </div>
-          <div v-if="documentStore.document.phoneNumber">
-            <div>Phone number</div>
-            <div>{{ documentStore.document.phoneNumber }}</div>
-          </div>
-        </div>
+
       </div>
       <div class="p-4 flex-[7_1_0%]">
-        <div class="mb-4">
-          <div class="font-bold">
-            {{ documentStore?.document?.firstName }} {{ documentStore?.document?.lastName }}
-          </div>
-          <div>
-            {{ documentStore?.document?.position }}
-          </div>
-        </div>
+
       </div>
     </div>
   </div>
