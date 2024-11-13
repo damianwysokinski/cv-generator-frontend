e45947116e4171160205868548ead7526c340d3e
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-13 16:29:19 +0100

    create component for textarea

---
+++ components/TextAreaInput.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const model = defineModel();
+</script>
+
+<template>
+  <textarea v-model="model" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded border border-gray-300 focus:ring-blue-500 focus:border-blue-5000"></textarea>
+</template>
