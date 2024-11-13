478f68128df5a28104db719793f667186c93f9d3
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-13 18:13:55 +0100

    create button components

---
+++ components/DangerButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="text-red-600 bg-red-50 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded text-sm px-5 py-2.5 text-center">
+    <slot />
+  </button>
+</template>
---
+++ components/PrimaryButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="text-white bg-zinc-950 focus:outline-none font-medium rounded text-sm px-5 py-2.5 text-center">
+    <slot />
+  </button>
+</template>
