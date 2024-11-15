0eb482c06f3c463ff95265d39aa0c02e2dcb1649
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-16 00:10:37 +0100

    move resume preview to ResumePreview component

---
+++ components/dashboard/resume/ResumePreview.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import SimplicityTemplate from '@/components/templates/SimplicityTemplate.vue';
+
+const templates = [
+  { name: 'Simplicity', component: SimplicityTemplate }
+];
+
+const selectedTemplate = shallowRef(templates[0]);
+</script>
+
+<template>
+  <component :is="selectedTemplate.component" />
+</template>
+
+<style>
+#previewDocument ul {
+  @apply list-disc pl-5;
+}
+</style>
