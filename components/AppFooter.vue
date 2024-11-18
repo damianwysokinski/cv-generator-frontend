e9c00021fb3e2b6f04c933a101b1b1d33e1a4316
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-18 22:15:53 +0100

    remove links from footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,19 +5,7 @@
 <template>
   <footer class="bg-white border-t border-gray-200 px-4 lg:px-6 py-2.5">
     <div class="w-full mx-auto max-w-screen-xl py-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center">© {{ new Date().getFullYear() }} <NuxtLink to="/" class="hover:underline">CV Generator™</NuxtLink>. All Rights Reserved.
-    </span>
-      <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
-        <li>
-          <NuxtLink to="/" class="hover:underline me-4 md:me-6">About</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/" class="hover:underline me-4 md:me-6">Privacy Policy</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/" class="hover:underline">Contact</NuxtLink>
-        </li>
-      </ul>
+      <span class="text-sm text-gray-500 sm:text-center">© {{ new Date().getFullYear() }} <NuxtLink to="/" class="hover:underline">CV Generator™</NuxtLink>. All Rights Reserved.</span>
     </div>
   </footer>
 </template>
