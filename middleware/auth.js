b2397b6b8d45f51eadc8eabcd7b0b8fb095980d2
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-18 19:02:18 +0100

    fix redirection if access token expired

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,9 +3,7 @@ import { useAuthStore } from "~/stores/auth.js";
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const authStore = useAuthStore();
 
-    if (!authStore.isLoggedIn) {
-        await authStore.checkAuth();
-    }
+    await authStore.checkAuth();
 
     if (!authStore.isLoggedIn) {
         return navigateTo('/auth/login');
