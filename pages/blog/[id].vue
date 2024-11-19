0dd11c59fc468ad55fb18f41ac2c70b424b38dea
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-19 16:33:48 +0100

    create pages for blog

---
+++ pages/blog/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-full mx-auto max-w-screen-xl py-8 lg:text-left">
+    <h1 class="mb-8 text-3xl font-bold leading-none text-gray-900 md:text-5xl"></h1>
+  </div>
+</template>
---
+++ pages/blog/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="px-4 lg:px-6">
+    <div class="w-full mx-auto max-w-screen-xl py-8 lg:text-left">
+      <h1 class="mb-8 text-3xl font-bold leading-none text-gray-900 md:text-5xl">Blog</h1>
+
+      <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+
+      </div>
+    </div>
+  </section>
+</template>
