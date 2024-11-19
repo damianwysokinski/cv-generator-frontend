dee107391fabdbbf19728c413288b8efd93ea592
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-19 16:34:29 +0100

    move Banner component to default template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,14 @@
+<script setup lang="ts">
+import BannerSection from "~/components/BannerSection.vue";
+</script>
+
 <template>
   <div>
     <app-header />
     <div>
       <slot />
     </div>
+    <banner-section />
     <app-footer />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import HeroSection from "~/components/homepage/HeroSection.vue";
-import BannerSection from "~/components/homepage/BannerSection.vue";
 </script>
 
 <template>
   <hero-section />
-  <banner-section />
 </template>
