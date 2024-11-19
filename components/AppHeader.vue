a556d376b89be8320d8ede1668487c914b61d757
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-11-19 11:11:34 +0100

    remove mr-2 from 'Get started' button

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -11,7 +11,7 @@
         </NuxtLink>
         <div class="flex items-center lg:order-2">
           <NuxtLink to="/auth/login" class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Log in</NuxtLink>
-          <NuxtLink to="/auth/register" class="text-white bg-zinc-950 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Get started</NuxtLink>
+          <NuxtLink to="/auth/register" class="text-white bg-zinc-950 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 focus:outline-none">Get started</NuxtLink>
         </div>
       </div>
     </nav>
